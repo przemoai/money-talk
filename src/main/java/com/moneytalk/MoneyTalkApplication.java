1749d6bc7a4c2c9c270c6c844260fca0e1de3990
pmroczek <again364@gmail.com>
2024-02-26 20:13:23 +0100

    init commit

---
+++ src/main/java/com/moneytalk/MoneyTalkApplication.java
@@ -0,0 +1,13 @@
+package com.moneytalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moneytalk/MoneyTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneytalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
